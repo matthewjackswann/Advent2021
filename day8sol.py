29e91a04f9348b6dd0d3207947a7d97b51ec22a0
Matthew Swann <cv20549@bristol.ac.uk>
2021-12-23 00:48:50 +0000

    Finaly finished day 8. Coming back to it, it was a lot easier then I thought

--- day8sol.py
+++ day8sol.py
@@ -34,12 +34,64 @@ print(total)
 
 # part 2
 
-# TODO: 
+f = open("day8.txt")
 
+total = 0
 
+for line in f:
+    possibleMapping = {}
+    for c in "abcdefg":
+        possibleMapping[c] = [c for c in "abcdefg"]
+    input, output = line.strip().split(" | ")
+    digits = (input + " " + output).split(" ")
+    # start of logic
+    lengths = {}
+    for digit in digits:
+        lengths[len(digit)] = lengths.get(len(digit), []) + ["".join(sorted(digit))]
+        for num in unique:
+            if len(digit) == len(num):
+                for s in digit:
+                    possibleMapping[s] = [v for v in possibleMapping[s] if v in num]
+    for l in lengths:
+        lengths[l] = list(set(lengths[l]))
+    if 3 in lengths and 2 in lengths:
+        aMapping = list(set(lengths[3][0]) - set(lengths[2][0]))[0]
+        for m in possibleMapping:
+            if 'a' in possibleMapping[m]: possibleMapping[m].remove('a')
+        possibleMapping[aMapping] = ['a']
+    if 4 in lengths and 2 in lengths:
+        bdMappings = list(set(lengths[4][0]) - set(lengths[2][0]))
+        for m in possibleMapping:
+            possibleMapping[m] = [n for n in possibleMapping[m] if (n != 'b' and n != 'd') or (m in bdMappings)]
+        for s in bdMappings:
+            possibleMapping[s] = [n for n in possibleMapping[s] if n == 'b' or n == 'd']
+    if 5 in lengths and len(lengths[5]) > 1:
+        adgMappings = set("abcdefg")
+        for digit in lengths[5]:
+            adgMappings = adgMappings.intersection(digit)
+        for s in adgMappings:
+            possibleMapping[s] = [n for n in possibleMapping[s] if n == 'a' or n == 'd' or n == 'g']
+    if 6 in lengths and len(lengths[6]) > 1:
+        abfgMappings = set("abcdefg")
+        for digit in lengths[6]:
+            abfgMappings = abfgMappings.intersection(digit)
+        for s in abfgMappings:
+            possibleMapping[s] = [n for n in possibleMapping[s] if n == 'a' or n == 'b' or n == 'f' or n == 'g']
+    while any(len(possibleMapping[m]) != 1 for m in possibleMapping):
+        for m in possibleMapping:
+            value = possibleMapping[m][0]
+            if len(possibleMapping[m]) == 1: # remove all other occurrences
+                for n in possibleMapping:
+                    if n != m:
+                        if value in possibleMapping[n]: possibleMapping[n].remove(value)
+    # all mappings are done
+    result = ""
+    for digit in output.split(" "):
+         segments = set(map(lambda d: possibleMapping[d][0] ,digit))
+         for value, s in enumerate(allNums):
+             if set(s) == segments:
+                 result += str(value)
+    total += int(result)
+f.close()
 
-
-
-
-
-# stuff
+print(total)
